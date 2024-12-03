924cefea302892f7a1ad01c8b9b6ea5b9a261188
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2024-12-03 20:11:09 -0300

    Display correct elements

--- front-end/src/pages/MarkdownPreviewer/index.tsx
+++ front-end/src/pages/MarkdownPreviewer/index.tsx
@@ -1,5 +1,6 @@
 import CodeMirror from '@uiw/react-codemirror'
 import { EditorView } from '@uiw/react-codemirror'
+import './style.css'
 
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
 
@@ -7,8 +8,23 @@ export default function MarkdownPreview() {
   return (
     <CodeMirror
       extensions={[
-        markdown({ base:markdownLanguage }),
+        markdown({ base: markdownLanguage }),
         EditorView.lineWrapping,
+        EditorView.theme({
+          '.cm-scroller': {
+            overflow: 'hidden',
+            fontFamily: '"Roboto", sans-serif',
+          },
+          '.cm-lineNumbers': {
+            display: 'none',
+          },
+          '.cm-gutters': {
+            display: 'none',
+          },
+          '.cm-activeLine': {
+            backgroundColor: 'unset',
+          },
+        })
       ]}
       />
   )
