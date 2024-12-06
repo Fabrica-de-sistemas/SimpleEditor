8fe17f08af4893eaf98a51d79cad4d3bdcf2f9d2
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2024-12-06 19:54:28 -0300

    Lazy loading initial implementation

--- front-end/src/pages/Editor.tsx
+++ front-end/src/pages/Editor.tsx
@@ -1,5 +1,13 @@
-import MarkdownPreview from "./MarkdownPreviewer";
+//import MarkdownPreview from "./MarkdownPreviewer";
+
+import React, { Suspense } from "react"
+
+const Mark = React.lazy(() => import("./MarkdownPreviewer"))
 
 export default function Simple() {
-    return <MarkdownPreview/>
+    return (
+        <Suspense>
+            <Mark/>
+        </Suspense>
+    )
 }
--- front-end/src/pages/MarkdownPreviewer/index.tsx
+++ front-end/src/pages/MarkdownPreviewer/index.tsx
@@ -1,12 +1,19 @@
-import CodeMirror from '@uiw/react-codemirror'
+import CodeMirror, { ViewUpdate } from '@uiw/react-codemirror'
 import { EditorView } from '@uiw/react-codemirror'
 import './style.css'
 
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
+import { useCallback, useState } from 'react'
 
 export default function MarkdownPreview() {
+  const [value, setValue] = useState("")
+  const onChange = useCallback((val: string, _viewUpdate: ViewUpdate) => {
+     setValue(val)
+   }, [])
   return (
     <CodeMirror
+      value={value}
+      onChange={onChange}
       extensions={[
         markdown({ base: markdownLanguage }),
         EditorView.lineWrapping,
