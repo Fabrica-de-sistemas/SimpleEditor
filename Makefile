6aa64455bd13e355996972ebcfd199bb1ca0b0de
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2024-12-14 18:15:12 -0300

    Tentativa 1 de omição de marcadores

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ npmoffline:
 live:
 	npm run dev --prefix ./front-end
 build: npmbuild
-	go build
+	go build -o bin/SimpleEditor ./cmd/main.go
 gobuild: npmbuild
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/SimpleEditor ./cmd/main.go
 winbuild: npmbuild
--- front-end/src/pages/MarkdownPreviewer/index.tsx
+++ front-end/src/pages/MarkdownPreviewer/index.tsx
@@ -1,20 +1,69 @@
-import CodeMirror, { ViewUpdate } from '@uiw/react-codemirror'
+import CodeMirror, { Decoration, Range, ViewUpdate } from '@uiw/react-codemirror'
 import { EditorView } from '@uiw/react-codemirror'
 import './style.css'
 
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
 import { useCallback, useState } from 'react'
-import { RegExpCursor } from '@codemirror/search'
+import { syntaxTree } from '@codemirror/language'
+import { SyntaxNodeRef } from '@lezer/common'
+
+const hideMark = Decoration.mark({
+  attributes: {
+    style: "color: red"
+  }
+})
 
 
-const rules = [
-  String.raw`\*{2}(.+?)\*{2}|_{2}(.+?)_{2}`, // "Bold"
-  String.raw`^(#{1,6})\s+(.+)$`, // "Headers"
-  // String.raw`\*{1}(.+?)\*{1}|_{1}(.+?)_{1}`, // "Italic"
-  String.raw`~{2}(.+?)~{2}`, // "Strikethrough"
-  // "`{1}(.+?)`{1}", // "Inlinecode"
-]
-
+function hide(view: EditorView) {
+  const marks: Range<Decoration>[] = []
+  let nodeBefore: SyntaxNodeRef | null = null
+  for (const {from, to} of view.visibleRanges) {
+    syntaxTree(view.state).iterate({
+      from, to,
+      enter: (node) => {
+        //console.log(node, node.name)
+        const pos = view.state.selection.main.head
+        /*console.log(node.type, node.name, node.node, view.state.doc.sliceString(node.from, node.to), 'TREE: ', node.tree, pos)
+        if (node.name == "StrongEmphasis" || node.name == "Emphasis") {
+          if (pos <= node.to && node.from <= pos) {
+            //console.log(`i'm here! pos: ${pos}; from: ${node.from}; to: ${node.to}`, node.name, node.tree)
+          } else {
+            marks.push(hideMark.range(node.from, node.to))
+          }
+          //console.log(node.type, view.state.doc.sliceString(node.from, node.to), node.tree)
+          }*/
+        if (!node.name.endsWith("Mark")) {
+          nodeBefore = node
+          return
+        }
+        if (nodeBefore == null) {
+          nodeBefore = node
+          return
+        }
+        if (nodeBefore.name !== node.name) {
+          nodeBefore = node
+          return
+        }
+        if (nodeBefore.from <= pos && pos <= node.to) {
+          nodeBefore = node
+          return
+        }
+        marks.push(
+          hideMark.range(nodeBefore.from, nodeBefore.to),
+          hideMark.range(node.from, node.to)
+        )
+        nodeBefore = node
+      },
+    })
+  }
+  return marks
+}
+/*
+const addDecor = StateEffect.define<{from: number, to: number}>({
+  map: ({from, to}, change) => ({
+    from: change.mapPos(from), to: change.mapPos(to)
+  })
+})*/
 
 export default function MarkdownPreview() {
 
@@ -22,12 +71,7 @@ export default function MarkdownPreview() {
   const onChange = useCallback((value: string, viewUpdate: ViewUpdate) => {
     setValue(value)
     console.log(viewUpdate.state.selection.main.head)
-    for (const rule of rules){
-      const cursor = new RegExpCursor(viewUpdate.state.doc, rule)
-      for (const c of cursor) {
-        console.log(c)
-      }
-    }
+    hide(viewUpdate.view)
   }, [])
   const onSelect = useCallback(()=>{},[])
   return (
