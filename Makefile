332dc0d1dbc830e8991643548da406bc78ec967c
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2025-01-14 08:57:39 -0300

    refactor: Migrando da biblioteca padrão para gofiber

--- Makefile
+++ Makefile
@@ -6,11 +6,14 @@ npmoffline:
 	npm run build --prefix ./front-end
 live:
 	npm run dev --prefix ./front-end
-build: npmbuild
+godependencies:
+	go get github.com/gofiber/fiber/v2
+	go mod tidy
+build: npmbuild godependencies
 	go build -o bin/SimpleEditor ./cmd/main.go
-gobuild: npmbuild
+gobuild: npmbuild godependencies
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/SimpleEditor ./cmd/main.go
-winbuild: npmbuild
+winbuild: npmbuild godependencies
 	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/SimpleEditor.exe ./cmd/main.go
 run: build
 	./bin/SimpleEditor
--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-	"SimpleEditor/cmd/api"
-	"SimpleEditor/cmd/middlaware"
 	frontend "SimpleEditor/front-end"
 	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	port := ":8080"
-	router := http.NewServeMux()
+	app := fiber.New()
+	app.Use(logger.New())
+	//router := http.NewServeMux()
 	//f := frontend.Export()
 
-	stack := middlaware.CreateStack(
-		middlaware.LoggerMiddlaware,
-	)
+	//stack := middlaware.CreateStack(
+	//	middlaware.LoggerMiddlaware,
+	//)
 
-	v1 := stack(api.V1())
+	//v1 := stack(api.V1())
 
 	//router.Handle("/", stack(http.FileServer(f)))
-	router.Handle("GET /", stack(http.HandlerFunc(frontend.HandleFrontEnd)))
-	router.Handle("GET /api/V1/*", v1)
-	router.Handle("POST /api/V1/*", v1)
-
+	//router.Handle("GET /", stack(http.HandlerFunc(frontend.HandleFrontEnd)))
+	//router.Handle("GET /api/V1/*", v1)
+	//router.Handle("POST /api/V1/*", v1)
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root:         frontend.Export(),
+		NotFoundFile: "index.html",
+	}))
 	fmt.Printf("Server runnig on port %s\n", port)
-	if err := http.ListenAndServe(":8080", router); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(app.Listen(port))
 }
