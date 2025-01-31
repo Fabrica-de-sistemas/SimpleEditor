c205cf0e2df33e8f812273ec6e75cb561e28c626
Mateus Henrique <mateushprofissional@gmail.com>
2025-01-31 07:32:22 -0300

    V0.2.1 - Adicionando logomarca 01bit.tech

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -6,21 +6,26 @@ import Simple from './pages/Editor'
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* Estrutura original (alterada por falta de design):
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index path='/' element={<Home/>}/>
-          <Route path='/editor' element={<Simple/>}/>
-          <Route path='*' element={<NotFound/>}/>
-        </Route>
-      </Routes> */}
-      <Routes>
-        <Route path='/' element={<Simple/>}>
-        </Route>
-        <Route path='/*' element={<NotFound/>}/>
-      </Routes>
-    </BrowserRouter>
+    <><BrowserRouter>
+    {/* Estrutura original (alterada por falta de design):
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index path='/' element={<Home/>}/>
+        <Route path='/editor' element={<Simple/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Route>
+    </Routes> */}
+    <Routes>
+      <Route path='/'  element={<Simple/>}>
+      </Route>
+      <Route path='*' element={<NotFound/>}/>
+      
+    </Routes>
+  </BrowserRouter>
+  <p style={{position: 'absolute', bottom: 0, textAlign: "center", width: "100%", opacity: 0.6}}>© Desenvolvido em colaboração com <a href="https://01bit.tech/pt">01bit.tech</a></p>
+  </>
+    
+    
   )
 }
 
